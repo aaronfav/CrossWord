dd7060cdadbaa6dec2222ee7dea6b4006d2bce46
ATella12 <ATella12@users.noreply.github.com>
2026-01-04 08:41:29 +0100

    Fix Farcaster embed meta URLs

--- app/head.tsx
+++ app/head.tsx
@@ -3,11 +3,11 @@ export default function Head() {
     <>
       <meta
         name="fc:miniapp"
-        content='{"version":"1","imageUrl":"https://cross-word-seven.vercel.app/embed.png","button":{"title":"Play Crossword","action":{"type":"launch_miniapp","url":"https://cross-word-seven.vercel.app","name":"Crossword"}}}'
+        content='{"version":"1","imageUrl":"https://cross-word-seven.vercel.app/embed.png","button":{"title":"Play Crossword","action":{"type":"launch_miniapp","url":"https://cross-word-seven.vercel.app/","name":"Crossword"}}}'
       />
       <meta
         name="fc:frame"
-        content='{"version":"1","imageUrl":"https://cross-word-seven.vercel.app/embed.png","button":{"title":"Play Crossword","action":{"type":"launch_frame","url":"https://cross-word-seven.vercel.app","name":"Crossword"}}}'
+        content='{"version":"1","imageUrl":"https://cross-word-seven.vercel.app/embed.png","button":{"title":"Play Crossword","action":{"type":"launch_frame","url":"https://cross-word-seven.vercel.app/","name":"Crossword"}}}'
       />
     </>
   );
